25e9cf451cb814238f6421ee88eb15efe516077e
sricke <sricke@uc.cl>
2024-04-26 11:05:13 -0400

    trying server config large files

--- server.sh
+++ server.sh
@@ -6,4 +6,5 @@ mlflow server \
   --port 8080 \
   --backend-store-uri $POSTGRESQL_URL \
   --artifacts-destination $STORAGE_URL \
-  --gunicorn-opts "--timeout 900"
+  --gunicorn-opts "--log-level debug --timeout 8000 --graceful-timeout 75 --keep-alive 3600" \
+  --serve-artifacts
